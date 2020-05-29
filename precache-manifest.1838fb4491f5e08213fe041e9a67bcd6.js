1fb73893b3fa3d6e94a3515ee5ab0f84bef3affc
Tetiana <ishchuktr@gmail.com>
2020-05-29 15:20:32 +0300

    Updates

---
+++ precache-manifest.1838fb4491f5e08213fe041e9a67bcd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e00fff0129e6c36519e1c244fc8ab7d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "16909792adaceab53da7",
+    "url": "./static/css/main.5835ff2f.chunk.css"
+  },
+  {
+    "revision": "c4584bd300c69b98e692",
+    "url": "./static/js/2.d929be01.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.d929be01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16909792adaceab53da7",
+    "url": "./static/js/main.8ad26d49.chunk.js"
+  },
+  {
+    "revision": "13d57103233adbc95222",
+    "url": "./static/js/runtime-main.7c3e9311.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  }
+]);
